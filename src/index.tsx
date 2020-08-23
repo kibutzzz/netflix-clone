9bcb52a1b7918b687e8bbf0cc20ef98da91e3f76
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2020-08-23 20:24:13 -0300

    normalization

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+  
+  * {
+    margin: 0;  
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    overflow-y: scroll;
+    overflow-x: hidden;
+    background-color: #222;
+  }
+`
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
